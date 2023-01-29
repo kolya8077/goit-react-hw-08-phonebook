6f766b117441ee4ea7f338c06bfafde6085b61d1
kolya8077 <kolya8077@gmail.com>
2023-01-29 19:44:25 +0200

    asdfgh

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -6,7 +6,6 @@ import storage from 'redux-persist/lib/storage';
 const initialContact = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
 ];
 
 
